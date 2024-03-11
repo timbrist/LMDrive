7ba3fabd8c439b25c7a7ffb42929360f6f83496c
yan <timbrist@outlook.com>
2024-03-11 15:15:16 +0200

    add get models script

---
+++ leaderboard/data/models/get_models.sh
@@ -0,0 +1,4 @@
+wget https://huggingface.co/OpenDILabCommunity/LMDrive-vision-encoder-r50-v1.0/resolve/main/vision-encoder-r50.pth.tar
+wget https://huggingface.co/OpenDILabCommunity/LMDrive-llava-v1.5-7b-v1.0/resolve/main/llava-v1.5-checkpoint.pth
+git lfs install
+git clone https://huggingface.co/liuhaotian/llava-v1.5-7b
